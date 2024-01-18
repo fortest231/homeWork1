1294aeb53969a4dfceca75f73e430dac41bcdee5
fortest231 <146324430+fortest231@users.noreply.github.com>
2024-01-18 19:20:09 +0600

    Initial Commit

---
+++ lesson3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson3
+//
+//  Created by MacBook Pro on 18/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
