a5d9e6976eeabc90e0a02203d2549be234bc3bbd
fortest231 <146324430+fortest231@users.noreply.github.com>
2024-01-22 14:35:52 +0600

    lesson3

--- lesson3/ViewController.swift
+++ lesson3/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+            
+        ])
+    
+
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("indexPath: \(indexPath.row)")
+        let cell = UITableViewCell(style: .subtitle,reuseIdentifier: "cell")
+        cell.textLabel?.text = "123213 21 "
+        cell.imageView?.image = UIImage(systemName: "star")
+        cell.detailTextLabel?.text = "detailTextLabel"
+        return cell
+    }
+    
+    
+}
